3459a49b7046066a00da4ad9440b35dfb263585b
Nikheel <nikheel.ranjan@gmail.com>
2024-06-05 22:09:50 +0530

    Setting up Project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # file-store-service
+
+# Getting Started
+
+### Reference Documentation
+This app uses the below tech stack:
+spring-boot=3.3.0
+JDK=17.x
+
+
---
+++ src/test/java/nikheel/rh/fss/FileStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nikheel.rh.fss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
